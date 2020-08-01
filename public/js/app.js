7aee10d6d212c7fcf12c020304a88b0b0ea9337b
José David Melian Villalobos <josedavidmelian@gmail.com>
2020-08-01 16:27:33 +0100

    front end template

--- public/js/app.js
+++ public/js/app.js
@@ -66,31 +66,41 @@ angular.module('PathFinding', [])
     $(document).ready(function(){
         var current_fs, next_fs, previous_fs; //fieldsets
         var opacity;
-        
+        var flag = true;
         $(".next").click(function(){
-        
-            current_fs = $(this).parent();
-            next_fs = $(this).parent().next();
+
+  
+            if($scope.user.password && $scope.user.rPassword && $scope.user.name
+                && $scope.user.lastname && $scope.user.email || flag){
+                if ($scope.user.password == $scope.user.rPassword || flag){
+                    current_fs = $(this).parent();
+                    next_fs = $(this).parent().next();
             
-            //Add Class Active
-            $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+                    //Add Class Active
+                    $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
             
-            //show the next fieldset
-            next_fs.show();
-            //hide the current fieldset with style
-            current_fs.animate({opacity: 0}, {
-                step: function(now) {
-                    // for making fielset appear animation
-                    opacity = 1 - now;
-                    
-                    current_fs.css({
-                    'display': 'none',
-                    'position': 'relative'
+                    //show the next fieldset
+                    next_fs.show();
+                    //hide the current fieldset with style
+                    current_fs.animate({opacity: 0}, {
+                        step: function(now) {
+                            // for making fielset appear animation
+                            opacity = 1 - now;
+                            
+                            current_fs.css({
+                            'display': 'none',
+                            'position': 'relative'
+                            });
+                            next_fs.css({'opacity': opacity});
+                        },
+                        duration: 600
                     });
-                    next_fs.css({'opacity': opacity});
-                },
-                duration: 600
-            });
+                }else {
+                    alert("Password doesn't math");
+                }
+            }else {
+                alert("Empty Fields");
+            }                    
         });
         
         $(".previous").click(function(){
@@ -120,10 +130,10 @@ angular.module('PathFinding', [])
             });
         });
         
-        $('.radio-group .radio').click(function(){
-            $(this).parent().find('.radio').removeClass('selected');
-            $(this).addClass('selected');
-        });
+        // $('.radio-group .radio').click(function(){
+        //     $(this).parent().find('.radio').removeClass('selected');
+        //     $(this).addClass('selected');
+        // });
         
         $(".submit").click(function(){
         return false;
@@ -132,4 +142,28 @@ angular.module('PathFinding', [])
         });
         /////////////////////////////////////////////////////////////////////////////////////////////////
         /////////////////////////////////////////////////////////////////////////////////////////////////
+
+        $scope.user = {
+            name: '',
+            lastname: '',
+            email: '',
+            password: '',
+            rPassword: ''
+        }
+
+        // $scope.register = function (){
+        //     console.log($scope.user);
+        //     if($scope.user.password && $scope.user.rPassword && $scope.user.name
+        //         && $scope.user.lastname && $scope.user.email){
+        //         if ($scope.user.password == $scope.user.rPassword){
+        //             $scope.nextStep();
+        //         }else {
+        //             alert("Password doesn't math");
+        //         }
+        //     }else {
+        //         alert("Empty Fields");
+        //     }
+        // }
+
+
 });
