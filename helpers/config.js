9392f94dfb759d53724e8a1b51bad059cd1b79ef
José David Melián Villalobos <josedavidmelian@gmail.com>
2020-08-02 20:15:17 +0100

    required heroku db

--- helpers/config.js
+++ helpers/config.js
@@ -1,5 +1,5 @@
 let config={
-    dbUrl: 'postgres://mezmtygjrnpavo:a48cf9f92146b0aa870a0f5f9e0cec305607cdba4b940983f6d9557807a7b85a@ec2-54-217-224-85.eu-west-1.compute.amazonaws.com:5432/d12pide2qkscqq' || 'postgres://postgres:masterkey@localhost:5432/PathFinding',
+    dbUrl: 'postgres://mezmtygjrnpavo:a48cf9f92146b0aa870a0f5f9e0cec305607cdba4b940983f6d9557807a7b85a@ec2-54-217-224-85.eu-west-1.compute.amazonaws.com:5432/d12pide2qkscqq',
     port:process.env.PORT || 3000
 }
 
